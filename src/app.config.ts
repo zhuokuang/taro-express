6e5b483281df9ee6cdcff86cafdf3a1406563237
zhuokuang <2183692898@qq.com>
2024-01-25 12:53:14 +0800

    feat: 修改tabBar

--- src/app.config.ts
+++ src/app.config.ts
@@ -18,7 +18,7 @@ export default defineAppConfig({
     list: [
       {
         pagePath: 'pages/index/index',
-        text: '首页',
+        text: '首页home',
       },
       {
         pagePath: 'pages/order/index',
